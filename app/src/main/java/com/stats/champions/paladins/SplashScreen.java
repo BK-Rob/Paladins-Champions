c7feb5ad59f1ba522bc4893bb72a028eabf24b6f
Maxime liege <liege30@gmail.com>
2017-04-27 01:41:38 +0200

    Splash + MainActivity with Observable tasks

---
+++ app/src/main/java/com/stats/champions/paladins/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.stats.champions.paladins;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        }, 4000);
+    }
+}
---
+++ app/src/main/java/com/stats/champions/paladins/api/Constants.java
@@ -0,0 +1,10 @@
+package com.stats.champions.paladins.api;
+
+public class Constants {
+
+    public static final String DEV_ID = "2008";
+    public static final String AUTH_KEY = "AE978BA9199F41D1A6525FFD8831821A";
+
+    public static final String BASE_URL = "http://api.paladins.com/paladinsapi.svc/";
+    public static String mSession = null;
+}
